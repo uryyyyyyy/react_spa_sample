a282d57ef6ab8db1131077ccd3c2cbf52b3119db
shiba <koki305@gmail.com>
2015-01-02 13:13:31 +0900

    add build task

--- gulpfile.js
+++ gulpfile.js
@@ -4,47 +4,53 @@ var uglify = require('gulp-uglify');
 var react = require('gulp-react');
 var rimraf = require('rimraf');
 var webserver = require('gulp-webserver');
+var rename = require ('gulp-rename');
+var usemin = require('gulp-usemin');
+var minifyCSS = require('gulp-minify-css');
 
-gulp.task('clean', function (cb) {
+gulp.task('cleanBuild', function (cb) {
     rimraf('./build', cb);
 });
 
-gulp.task('clean_js', function (cb) {
+gulp.task('cleanJs', function (cb) {
     rimraf('app/js', cb);
 });
 
-gulp.task('default', function(){
-    console.log('Hello World');
-});
-
-gulp.task('scripts', function() {
-    gulp.src(['./app/*.js'])
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./build/'))
-});
-
-gulp.task('compress', function() {
-    gulp.src('app/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/'))
-});
-
-gulp.task('react', ['clean_js'], function () {
+gulp.task('compileJsx', ['cleanJs'], function () {
     return gulp.src('app/jsx/**/*.js')
     .pipe(react())
     .pipe(gulp.dest('app/js/'));
 });
 
-gulp.task('clean2', ['clean'], function() {
-    // Something
+gulp.task('watchJsx', function () {
+    gulp.watch('app/jsx/**/*.js', ["compileJsx"]);
 });
 
+gulp.task('usemin', function() {
+    gulp.src('app/index.html')
+    .pipe(usemin({
+        js:[uglify()]
+    }))
+    .pipe(gulp.dest('build/'));
+});
 
-gulp.task('webserver', ['react'], function() {
-    gulp.src('./')
+gulp.task('build_', ['cleanBuild', 'compileJsx'], function () {
+    gulp.src('app/index.html')
+    .pipe(usemin({
+        js:[uglify()],
+        css:[minifyCSS()]
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('webServer', function() {
+    gulp.src('./app')
     .pipe(webserver({
         livereload: true,
-        directoryListing: true,
-        open: true
+        directoryListing: false,
+        open: false
     }));
 });
+
+gulp.task("server", ["webServer", "watchJsx"]);
+gulp.task("build", ["build_"])
